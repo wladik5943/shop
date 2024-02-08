7ff18c16a471c99b0254a0e3a96c125227ea7e98
wladi <wlad@>
2024-02-08 05:05:18 +0300

    serialize

--- src/main/java/by/teachmeskills/shop/main.java
+++ src/main/java/by/teachmeskills/shop/main.java
@@ -1,4 +1,19 @@
 package by.teachmeskills.shop;
 
+import by.teachmeskills.shop.entity.User;
+import by.teachmeskills.shop.repository.FileRepository;
+
 public class main {
+    public static void main(String[] args) {
+        FileRepository fileRepository = new FileRepository();
+
+        User user = new User(1,"vlad","wlad","1233","33221");
+        User user1 = new User(2,"dan","4332","6785","rop");
+
+
+        fileRepository.add(user);
+        fileRepository.add(user1);
+
+        System.out.println(fileRepository.allUsers().toString());
+    }
 }
--- src/main/java/by/teachmeskills/shop/repository/FileRepository.java
+++ src/main/java/by/teachmeskills/shop/repository/FileRepository.java
@@ -4,20 +4,43 @@ import by.teachmeskills.shop.entity.User;
 
 import java.io.*;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.stream.Stream;
 
 public class FileRepository implements ShopRepository, Serializable {
-
+    private Collection<User> users = new LinkedList<>();
+    private int count = 0;
     public void serialize(Collection<User> users, String path){
         try(ObjectOutputStream objOStr = new ObjectOutputStream(new FileOutputStream(path))){
-            objOStr.write(users.);
+            objOStr.writeObject((Object)users);
         }
         catch (IOException e){
-
+            new RuntimeException(e);
         }
     }
+
+    public Collection<User> deSerialize(String path) {
+
+        try{
+            FileInputStream fileInputStream = new FileInputStream(path);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Collection<User> users = new LinkedList<User>();
+
+        }
+        catch (IOException e){
+            new RuntimeException(e);
+        }
+        catch (ClassNotFoundException e){
+            new RuntimeException(e);
+        }
+        return users;
+    }
+
     @Override
     public void add(User user) {
-
+        users.add(user);
+        count++;
+        serialize(users,"D:\\progects\\shop\\src\\main\\resources\\res");
     }
 
     @Override
@@ -27,6 +50,8 @@ public class FileRepository implements ShopRepository, Serializable {
 
     @Override
     public Collection<User> allUsers() {
-        return null;
+        return deSerialize("D:\\progects\\shop\\src\\main\\resources\\res");
     }
+
+
 }
